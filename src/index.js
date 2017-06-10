e52f0029228991a6066a6d98bb89135f5d849bd7
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-06-09 23:08:18 -0400

    Add highlight.js css for code block color

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Docs from './docs/Docs';
 // import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import '../node_modules/highlight.js/styles/ocean.css';
 
 ReactDOM.render(
   <Docs />,
